dccbbd207f3d0f1723641ab2d95368702d55d2b4
imnewt <maitantruc1999@gmail.com>
2020-02-11 22:29:35 +0700

    import icons from third library

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 import HomeScreen from "./screens/Home"
 import BookListScreen from "./screens/BookList"
@@ -55,7 +56,28 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = 'md-home';
+            } else if (route.name === 'Cart') {
+                iconName = 'ios-cart';
+            } else if (route.name === 'Settings') {
+                iconName = 'ios-list';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
         <Tab.Screen name="Home" component={HomeStackScreen} />
         <Tab.Screen name="Cart" component={CartStackScreen} />
         <Tab.Screen name="Settings" component={SettingsStackScreen} />
--- screens/Home.js
+++ screens/Home.js
@@ -8,6 +8,8 @@ import {
   StyleSheet
 } from 'react-native';
 
+import Icon from "react-native-vector-icons/Ionicons"
+
 import Aoun from "../images/aoun.jpg"
 
 export default class Home extends Component {
@@ -17,6 +19,7 @@ export default class Home extends Component {
             <View style={styles.container}>
                 <Image source={Aoun} />
                 <Text style={{fontSize: 20}}>Test</Text>
+                <Icon name="ios-home" size={30} />
                 <Button
                     title="to book list"
                     onPress={() => navigation.navigate('BookList')}
